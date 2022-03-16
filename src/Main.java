3735f427cc675a33c27482d255590d520d9b2a75
Xuekun Zhou <xuekun@live.nl>
2022-03-16 14:20:13 +0100

    Readme

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        RouteCalc alg = new RouteCalc(5, 100, 3);
+        RouteCalc alg = new RouteCalc(100, 100, 3);
 
 //        alg.printDistances();
     }
